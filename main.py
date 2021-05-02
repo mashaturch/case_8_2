7d1b9b039bc90db0e697baf7ba85e7bc81ac87b8
mashaturch <s1yguvzg>
2021-05-02 21:01:43 +0700

    six functions

--- main.py
+++ main.py
@@ -34,19 +34,46 @@ def acceptCommand():
         acceptCommand()
 
 def moveUp():
-    """
-    path = os.getcwd()
-    old_dir = os.chdir(path)
-    print(os.getcwd())"""
-    # Переход на уровень выше
+    """Moving to a higher level"""
     os.chdir('..')
 
 def moveDown(currentDir):
+    """Moving to a level below"""
     if os.path.isdir(currentDir):
         os.chdir(currentDir)
     else:
         currentDir = input('Ошибка! Введите новый каталог снова: ')
         moveDown(currentDir)
+
+
+def countFiles(path):
+    """Counting the number of files"""
+    quantity = 0
+    for name in os.listdir(path):
+        if os.path.isdir(os.path.join(path, name)):
+            quantity += countFiles(os.path.join(path, name))
+        if os.path.isfile(os.path.join(path, name)):
+            quantity += 1
+    return quantity
+
+def count_fold_files(path):
+    """Counting the number of folders and files"""
+    quantity = 0
+    for name in os.listdir(path):
+        if os.path.isdir(os.path.join(path, name)):
+            quantity += countFiles(os.path.join(path, name))
+        quantity += 1
+    return quantity
+
+def countBytes(path):
+    """Counting the total size (in bytes) of all files"""
+    quantity_bytes = 0
+    for name in os.listdir(path):
+        if os.path.isdir(os.path.join(path, name)):
+            quantity_bytes += countFiles(os.path.join(path, name))
+        if os.path.isfile(os.path.join(path, name)):
+            quantity_bytes += os.path.getsize(os.path.join(path, name))
+    return quantity_bytes
 
 def runCommand(command):
     """Determination of the required function by the command number"""
@@ -71,6 +98,15 @@ def runCommand(command):
     if command == 3:
         currentDir = input('Введите нужный новый каталог: ')
         moveDown(currentDir)
+
+    if command == 4:
+        print('Количество файлов:', countFiles(path))
+        print('Количество каталогов:', count_fold_files(path) - countFiles(path))
+
+    if command == 5:
+        print ('Суммарный объем (в байтах) всех файлов:', countBytes(path))
+
+
 
     print()
 
